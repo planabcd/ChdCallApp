0026800b93fb93be49642060bc7a50d0d66a2299
planabcd <xin_xianyu@163.com>
2017-05-19 11:59:42 +0800

    修改教师端mac地址校验

--- app/src/main/java/com/xiaoniu/wifihotspotdemo/TeacherHomeActivity.java
+++ app/src/main/java/com/xiaoniu/wifihotspotdemo/TeacherHomeActivity.java
@@ -8,15 +8,10 @@ import android.widget.LinearLayout;
 
 import com.google.gson.Gson;
 import com.xiaoniu.wifihotspotdemo.common.BaseActivity;
-import com.xiaoniu.wifihotspotdemo.common.Constant;
 import com.xiaoniu.wifihotspotdemo.domain.Teacher;
 import com.xiaoniu.wifihotspotdemo.util.GsonBuilderUtil;
-import com.xiaoniu.wifihotspotdemo.util.NetWorkUtil;
 import com.xiaoniu.wifihotspotdemo.util.PrefUtils;
 import com.xiaoniu.wifihotspotdemo.util.UIUtil;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class TeacherHomeActivity extends BaseActivity implements View.OnClickListener {
 
@@ -37,8 +32,8 @@ public class TeacherHomeActivity extends BaseActivity implements View.OnClickLis
 
     private void initView() {
         String json = PrefUtils.getString(this, "teacher", null);
-        if(TextUtils.isEmpty(json)){
-            UIUtil.showToast(this,"登录失效,请重新登录");
+        if (TextUtils.isEmpty(json)) {
+            UIUtil.showToast(this, "登录失效,请重新登录");
             Intent it = new Intent(this, LoginActivity.class);
             startActivity(it);
             finish();
@@ -87,56 +82,52 @@ public class TeacherHomeActivity extends BaseActivity implements View.OnClickLis
      * 生成考勤信息二维码
      */
     private void setEr() {
-        UIUtil.okTips(this,"稍等哦","火速开发ing");
+        UIUtil.okTips(this, "稍等哦", "火速开发ing");
     }
-
-
 
 
     /**
      * 个人信息
      */
     private void personInfo() {
-        UIUtil.okTips(this,"个人信息","工号:"+mTeacher.getTeacherId()+"\n姓名:"+mTeacher.getName());
+        UIUtil.okTips(this, "个人信息", "工号:" + mTeacher.getTeacherId() + "\n姓名:" + mTeacher.getName());
     }
 
     /**
      * 教师进入考勤配置界面
      */
     private void startCall() {
-        final String macAddress = mTeacher.getMacAddress();
-        if(TextUtils.isEmpty(macAddress)){
-            Map<String,String> params = new HashMap<String,String>();
-            params.put("teacherId",mTeacher.getTeacherId()+"");
-            NetWorkUtil.post(Constant.URL_TEACHER_GET_MACADDRESS, params, new NetWorkUtil.Worker() {
-                @Override
-                public void success(String result, Gson gson) {
-                    Teacher teacher = gson.fromJson(result, Teacher.class);
-                    String netMacAddress = teacher.getMacAddress();
-                    if(!TextUtils.isEmpty(netMacAddress)){
-                        Intent it = new Intent(TeacherHomeActivity.this,TeacherSettingActivity.class);
-                        startActivity(it);
-                    }else{
-                        UIUtil.alert(TeacherHomeActivity.this, "请先绑定mac地址","如果不绑定mac地址无法进行考勤", new UIUtil.AlterCallBack() {
-                            @Override
-                            public void confirm() {
-                                Intent it = new Intent(TeacherHomeActivity.this,TeacherSettingActivity.class);
-                                startActivity(it);
-                            }
-                        });
-                    }
-                }
-            });
+        String json = PrefUtils.getString(this, "teacher", null);
+        if (TextUtils.isEmpty(json)) {
+            UIUtil.showToast(this, "登录失效,请重新登录");
+            Intent it = new Intent(this, LoginActivity.class);
+            startActivity(it);
+            finish();
             return;
         }
-        Intent it = new Intent(this,TeacherPreAttenceActivity.class);
-        startActivity(it);
+        Gson gson = GsonBuilderUtil.create();
+        mTeacher = gson.fromJson(json, Teacher.class);
+
+        String macAddress = mTeacher.getMacAddress();
+        if (TextUtils.isEmpty(macAddress)) {
+            UIUtil.alert(TeacherHomeActivity.this, "请先绑定mac地址", "如果不绑定mac地址无法进行考勤", new UIUtil.AlterCallBack() {
+                @Override
+                public void confirm() {
+                    Intent it = new Intent(TeacherHomeActivity.this, TeacherSettingActivity.class);
+                    startActivity(it);
+                }
+            });
+        } else {
+            Intent it = new Intent(this, TeacherPreAttenceActivity.class);
+            startActivity(it);
+        }
     }
+
     /**
      * 其他设置
      */
     private void setting() {
-        Intent it = new Intent(this,TeacherSettingActivity.class);
+        Intent it = new Intent(this, TeacherSettingActivity.class);
         startActivity(it);
     }
 
@@ -147,7 +138,6 @@ public class TeacherHomeActivity extends BaseActivity implements View.OnClickLis
         Intent intent = new Intent(this, TeacherHistoryActivity.class);
         startActivity(intent);
     }
-
 
 
 }
